8b8492c830b35b11fe363cae127a779562f5469f
Tin Aung Khant <evil.bloodelf@gmail.com>
2020-03-17 10:42:46 +0630

    Your Comment

---
+++ app/src/main/java/com/example/mymagicball/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.mymagicball;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay=findViewById(R.id.image_EightBall);
+        Button askButton=findViewById(R.id.askButton);
+        final int[] ballArray={R.drawable.ball1,R.drawable.ball2,R.drawable.ball3,R.drawable.ball4,R.drawable.ball5};
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random myrandom=new Random();
+                int number=myrandom.nextInt(5);
+                ballDisplay.setImageResource(ballArray[number]);
+            }
+        });
+    }
+}
